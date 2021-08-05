42e2191bb42ac3c8f852e6ee2f3edd49d916c62d
Andrey Liporage <andreyliporage@pop-os.localdomain>
2021-08-04 21:48:12 -0300

    graficos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OlympicsAnalytics';
+
+  paises = [
+    { name: "China", value: 70, ouro: 32, prata: 22, bronze: 16 },
+    { name: "Estados Unidos", value: 79, ouro: 25, prata: 31, bronze: 23 },
+    { name: "Japão", value: 40, ouro: 21, prata: 7, bronze: 12 },
+    { name: "Grã-Bretanha", value: 48, ouro: 15, prata: 18, bronze: 15 },
+    { name: "Austrália", value: 36, ouro: 15, prata: 4, bronze: 17 },
+    { name: "ROC", value: 53, ouro: 14, prata: 21, bronze: 18 },
+    { name: "Alemanha", value: 33, ouro: 9, prata: 8, bronze: 16 },
+    { name: "França", value: 25, ouro: 6, prata: 10, bronze: 9 },
+    { name: "Itália", value: 31, ouro: 6, prata: 9, bronze: 16 },
+    { name: "Países Baixos", value: 23, ouro: 6, prata: 8, bronze: 9 },
+  ];
+
+  onSelect(event: any) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,12 @@ import { TopBarComponent } from './components/top-bar/top-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
